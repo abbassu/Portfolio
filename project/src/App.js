c9fff6e10c6d5e3f8d4f0fa079c6bcd17fc2ecb3
abbassu <abbassurakji99@gmail.com>
2023-04-29 22:45:51 +0300

    add navbar

--- project/src/App.js
+++ project/src/App.js
@@ -1,64 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
-
-
+import Home from './component/home/home';
 
 function App() {
-  const [sff,ss]=useState([])
-  const api="a49bf4b57b3b4d9192b81ad9d7d00596"
-  // sss="https://api.spoonacular.com/recipes/716429/information?apiKey=${api}"
- async function aa(){
-
-    
- const apiy=  await fetch(`https://api.spoonacular.com/recipes/random?number=100&apiKey=${api}`)
-      const data= await apiy.json()
-
-      
-        console.log(data.recipes)
-        ss(data.recipes)
-      
-
-
-      
-      
-      
-      
-        
-      
-      
-      // .catch(err => console.error(err));
-  }
-  
-  useEffect(()=>{
-
-    console.log("in effect")
-   aa()
-    
-  },[])
-
-
   return (
     <div className="App">
-      <h1>
-        
-      fffffffff
-      </h1> 
-      <div className='all-food' >
-
-      
-{
-  sff.map((recipes,index)=>{
-    return(
-      <div>
-      <div> {recipes.title} </div>
-      <img src={recipes.image} alt="" className='image' />
-      </div>
-
-    )
-  })
-}
-</div>
+      <Home/>
     </div>
   );
 }
--- project/src/component/navbar/navbar.js
+++ project/src/component/navbar/navbar.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useContext } from "react";
+import { Outlet,Link } from "react-router-dom";
+import "./navbar.scss"
+
+import logo from "./ve.jpg"
+
 function Navbar(){
+
+    const [open,setNotOpen]=useState(true)
     return(
-        <div>
-            Navbar
-        </div>
+    <>
+          <div className="navigationn">
+            <Link className="logo-container" to={"/"}>
+             <img src={logo} alt="" />
+            </Link>
+            <div className="nav-links-containerrr" >
+            <div className={`linkss  ${open ? "":"jal" } `} onClick={()=>{
+                setNotOpen(!open)
+            }}  >
+            <Link className="nav-linkk" to={"/"}> HOME </Link>
+            <Link className="nav-linkk" to={"operation"}> About Us</Link>
+            <Link className="nav-linkk" to={"orders"}>Contact</Link>
+            <Link className="nav-linkk" to={"shop"}> Projects</Link>
+            </div>
+            <i className="fa-solid fa-bars" onClick={()=>{
+                setNotOpen(!open)
+            }}></i>
+            </div>
+      </div>
+    </>
     )
-}export default Navbar
+}
+export default Navbar
